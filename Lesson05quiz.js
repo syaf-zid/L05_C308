2414cec10d365a121d41b58bb746a26e740d9f5c
syafzid <18052270@myrp.edu.sg>
2019-11-18 15:36:33 +0800

    completed quiz

--- Lesson05quiz.js
+++ Lesson05quiz.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, Image, ScrollView} from 'react-native';
+
 //INSERT DATA HERE
+const PET_DATA = {
+  name: 'Gizmo',
+  age: '5',
+  image: require('./src/img/bird.jpg'),
+};
+
+const OWNER_DATA = {
+  name: 'Daniyah Bai',
+  contact: '97502219',
+  membership: 'gold',
+};
 
 const Lesson05quiz = () => {
   return (
-    <View>
+    <ScrollView>
+      <Text>Name:</Text>
+      <Text>----------{PET_DATA.name}</Text>
+      <Text>Age:</Text>
+      <Text>----------{PET_DATA.age} years old</Text>
+      <Image source={PET_DATA.image} />
 
-    </View>
+      <Text>Owner Information</Text>
+      <Text>--------------------</Text>
+      <Text>Name:</Text>
+      <Text>----------{OWNER_DATA.name}</Text>
+      <Text>Age:</Text>
+      <Text>----------{OWNER_DATA.contact}</Text>
+      <Text>Membership:</Text>
+      <Text>----------{OWNER_DATA.membership}</Text>
+    </ScrollView>
   );
 };
 export default Lesson05quiz;
