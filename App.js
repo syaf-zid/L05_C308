103299debd59dcf14c7f7e21b0764664b530e891
syafzid <18052270@myrp.edu.sg>
2019-11-18 12:46:28 +0800

    done with lesson

--- App.js
+++ App.js
@@ -3,11 +3,19 @@
  */
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, ScrollView} from 'react-native';
 
 const MOVIES_DATA = [
-  {title: 'Doctor Sleep', year: '2019'},
-  {title: 'Midway', year: '2019'},
+  {
+    title: 'Doctor Sleep',
+    year: '2019',
+    poster: require('./src/img/doctor-sleep.jpg'),
+  },
+  {
+    title: 'Midway',
+    year: '2019',
+    poster: require('./src/img/midway.jpg'),
+  },
 ];
 
 const App = () => {
@@ -16,12 +24,13 @@ const App = () => {
     let movie = MOVIES_DATA[i];
     moviesArr.push(<Text key={movie.title}>{movie.title}</Text>);
     moviesArr.push(<Text key={movie.year}>{movie.year}</Text>);
+    moviesArr.push(<Image source={movie.poster} />);
     console.log(moviesArr);
   }
   return (
-    <View>
+    <ScrollView>
       <View>{moviesArr}</View>
-    </View>
+    </ScrollView>
   );
 };
 
