8f1b521d678f7daa24a59f251ac0a6517e1593e3
syafzid <18052270@myrp.edu.sg>
2019-11-18 11:51:46 +0800

    Activity 1- 3

--- App.js
+++ App.js
@@ -5,10 +5,26 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+// let movie = {title: 'Doctor Sleep', year: '2019'};
+const MOVIES_DATA = ['Doctor Strange', 'Midway'];
+
 const App = () => {
+  let moviesArr = [];
+  for (let i = 0; i < MOVIES_DATA.length; i++) {
+    let movie = MOVIES_DATA[i];
+    moviesArr.push(<Text key={movie}>{movie}</Text>);
+    console.log(moviesArr);
+  }
   return (
     <View>
-      <Text>C308 Web Frameworks</Text>
+      {/*<Text>C308 Web Frameworks</Text>*/}
+      {/*<Text>{movie.title}</Text>*/}
+      {/*<Text>{movie.year}</Text>*/}
+
+      {/*<Text key="Doctor Sleep">Doctor Sleep</Text>*/}
+      {/*<Text key="2019">2019</Text>*/}
+
+      <View>{moviesArr}</View>
     </View>
   );
 };
