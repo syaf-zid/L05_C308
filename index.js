cdb847fbf8edd439b86aa01eaeb707778409cc81
syafzid <18052270@myrp.edu.sg>
2019-11-18 12:58:43 +0800

    done with exercise

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Getaboatapp from './Getaboatapp';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Getaboatapp);
