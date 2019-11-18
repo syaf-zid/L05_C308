f20d0823723cff08d7bde92beae2c1fbfc247a2e
syafzid <18052270@myrp.edu.sg>
2019-11-18 12:01:29 +0800

    done with recap

--- App.js
+++ App.js
@@ -5,25 +5,21 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-// let movie = {title: 'Doctor Sleep', year: '2019'};
-const MOVIES_DATA = ['Doctor Strange', 'Midway'];
+const MOVIES_DATA = [
+  {title: 'Doctor Sleep', year: '2019'},
+  {title: 'Midway', year: '2019'},
+];
 
 const App = () => {
   let moviesArr = [];
   for (let i = 0; i < MOVIES_DATA.length; i++) {
     let movie = MOVIES_DATA[i];
-    moviesArr.push(<Text key={movie}>{movie}</Text>);
+    moviesArr.push(<Text key={movie.title}>{movie.title}</Text>);
+    moviesArr.push(<Text key={movie.year}>{movie.year}</Text>);
     console.log(moviesArr);
   }
   return (
     <View>
-      {/*<Text>C308 Web Frameworks</Text>*/}
-      {/*<Text>{movie.title}</Text>*/}
-      {/*<Text>{movie.year}</Text>*/}
-
-      {/*<Text key="Doctor Sleep">Doctor Sleep</Text>*/}
-      {/*<Text key="2019">2019</Text>*/}
-
       <View>{moviesArr}</View>
     </View>
   );
