0695e248d15417815da115fd769dd2b528690478
syafzid <18052270@myrp.edu.sg>
2019-11-18 15:25:41 +0800

    quiz

---
+++ Lesson05quiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+//INSERT DATA HERE
+
+const Lesson05quiz = () => {
+  return (
+    <View>
+
+    </View>
+  );
+};
+export default Lesson05quiz;
