939dc7b1dd512aa7f583d9722da45f2533117e80
syafzid <18052270@myrp.edu.sg>
2019-11-18 10:11:41 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Lesson 05 Activities
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>C308 Web Frameworks</Text>
+    </View>
+  );
+};
+
+export default App;
